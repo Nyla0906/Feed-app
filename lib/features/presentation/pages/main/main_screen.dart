5c11adb4340efa33d4e4794363fb8a478cb59d8c
nyla0212 <atolqinov823@gmail.com>
2025-09-18 23:32:29 +0500

    implement main screen

---
+++ lib/features/presentation/pages/main/main_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mircos/features/presentation/pages/favourites/favourite_page.dart';
+import 'package:mircos/features/presentation/pages/feed/feed_screen.dart';
+import 'package:mircos/features/presentation/pages/feed/bloc/feed_bloc.dart';
+import 'package:mircos/features/presentation/pages/feed/bloc/feed_event.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [const FeedScreen(), const FavouritePage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => FeedBloc()..add(LoadFeedEvent()),
+      child: Scaffold(
+        body: _pages[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.grey,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Feed'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favourites',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
